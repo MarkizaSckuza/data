6e244b1f89c794fab937e45d85be111c2d743ce5
Eugene Perchekliy <eperchekliy@infosoftplus.com>
2016-10-25 23:21:58 +0300

    Case 4 (Fix Routes panel resize.) implementation.

--- cip-geo-data-generator/src/main/resources/webapp/js/ext/ui/RoutesGrid.js
+++ cip-geo-data-generator/src/main/resources/webapp/js/ext/ui/RoutesGrid.js
@@ -2,11 +2,12 @@ Ext.define("DataGenerator.ui.RoutesGrid", {
     extend: 'Ext.grid.Panel',
     id: 'routes-grid',
     columns: [
-        { text: 'Route #',  dataIndex: 'id', width: 80 },
+        { text: 'Route #',  dataIndex: 'id', width: '20%' },
         {
             text: 'Points',
             dataIndex: 'points',
             flex: 1,
+            width: '80%',
 
             renderer: function(value) {
                 return value.map(function(elem){
